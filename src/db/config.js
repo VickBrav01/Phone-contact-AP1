fa4a5318ae9b9fc8ceae59136db163cb536fc62a
kemboi590 <bkemboi590@gmail.com>
2023-06-14 21:58:26 +0300

    done

--- src/db/config.js
+++ src/db/config.js
@@ -4,20 +4,25 @@ import assert from "assert";
 
 dotenv.config();
 
-const { HOST_URL, HOST, PORT, SQL_SERVER, SQL_USER, SQL_PWD, SQL_DB } =
-  process.env;
+const {
+  HOST_URL,
+  HOST,
+  PORT,
+  SQL_SERVER,
+  SQL_USER,
+  SQL_PWD,
+  SQL_DB,
+} = process.env;
 
 const sqlEncrypt = process.env.SQL_ENCRYPT === "true";
 
-assert(HOST, "HOST_URL is required");
-assert(PORT, "HOST is required");
+assert(PORT, "HOST_URL is required");
+assert(HOST, "HOST is required");
 
-export const config = {
+const config = {
   port: PORT,
   host: HOST,
-
   url: HOST_URL,
-  server: SQL_SERVER,
   sql: {
     server: SQL_SERVER,
     user: SQL_USER,
@@ -25,7 +30,9 @@ export const config = {
     database: SQL_DB,
     options: {
       encrypt: sqlEncrypt,
-      enableArithAbort: true,
+      enableArithAbort: true
     },
   },
 };
+
+export default config;
