2ead97a7aa3be594117477b4edb10297a6154c87
Gideonkipyegon <gideonkipyegon837@gmail.com>
2023-06-15 15:28:12 +0300

    bcrypt and jsonwebtoken

--- src/db/config.js
+++ src/db/config.js
@@ -5,13 +5,13 @@ import assert from "assert";
 dotenv.config();
 
 const {
-  HOST_URL,
-  HOST,
-  PORT,
-  SQL_SERVER,
-  SQL_USER,
-  SQL_PWD,
-  SQL_DB,
+    HOST_URL,
+    HOST,
+    PORT,
+    SQL_SERVER,
+    SQL_USER,
+    SQL_PWD,
+    SQL_DB,
 } = process.env;
 
 const sqlEncrypt = process.env.SQL_ENCRYPT === "true";
@@ -20,19 +20,19 @@ assert(PORT, "HOST_URL is required");
 assert(HOST, "HOST is required");
 
 const config = {
-  port: PORT,
-  host: HOST,
-  url: HOST_URL,
-  sql: {
-    server: SQL_SERVER,
-    user: SQL_USER,
-    password: SQL_PWD,
-    database: SQL_DB,
-    options: {
-      encrypt: sqlEncrypt,
-      enableArithAbort: true
+    port: PORT,
+    host: HOST,
+    url: HOST_URL,
+    sql: {
+        server: SQL_SERVER,
+        user: SQL_USER,
+        password: SQL_PWD,
+        database: SQL_DB,
+        options: {
+            encrypt: sqlEncrypt,
+            enableArithAbort: true
+        },
     },
-  },
 };
 
 export default config;
